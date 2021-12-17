4b18a5c7abadb133c7bc06f8d1a81b599a1837d4
Linuxpert <g.manfre10@gmail.com>
2021-12-17 14:27:46 +0100

    added some changes

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body style="background-color:purple">
     <?php
-        $hello = "Hello world"
+        $hello = "Supercalifragilistichespiralitoso"
     ?>
 
-    <h1> <?php echo $hello . ' ' . strlen($hello) ?></h1>
+    <h1 style="color:white; text-align:center"> <?php echo $hello . ' : questa frase &#232; lunga ' . strlen($hello) . " lettere" ?></h1>
 
 
 </body>
